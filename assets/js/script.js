ac73e061bf330b374e7fb144b3a8cf54a5c59abb
Melek Gharbi <gharbimelek92@gmail.com>
2021-04-04 19:04:27 +0100

    added content on about and service page

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("Invalid Email");
                 return false;
             }
-            $.ajax({
-                url : "https://docs.google.com/forms/u/2/d/e/1FAIpQLSeTqPBSWRxrrpHOCev8O-f3iq9C_8OmScf-mm1tAjM0vbe9dQ/formResponse",
-                data : $("#contact-form").serialize(),
-                type: "POST",
+            $.ajax({ // code from https://edcupaioli.com/blog/google-form-front-end/
+                url : "https://docs.google.com/forms/u/2/d/e/1FAIpQLSeTqPBSWRxrrpHOCev8O-f3iq9C_8OmScf-mm1tAjM0vbe9dQ/formResponse", //The public Google Form url, but replace /view with /formResponse
+                data : $("#contact-form").serialize(), //Nifty jquery function that gets all the input data
+                type: "POST", //tells ajax to post the data to the url
                 dataType: "json",
-                statusCode : {
-                    0: function(data) {
+                statusCode : { //the status code from the POST request
+                    0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
+                        //success
                         $("#form-success").text('Hooray');
                     },
-                    200: function(data) {
+                    200: function(data) { //200 is a success code. it went through!
                         $("#form-success").text('Hooray');
-                    },
+                    }, //success
                     403: function(data) {
+                        //error
                         alert('Oh no ! Error on submission')
                     }
                 }
